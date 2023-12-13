048edcc363aa0fc56cda8ecd48a5b8172af218e8
Germán Faller <faller222@gmail.com>
2023-12-13 19:39:05 -0300

    Init - Backend

---
+++ backend/src/main/java/com/sweetcherrytech/faller/FullstackTestApplication.java
@@ -0,0 +1,13 @@
+package com.sweetcherrytech.faller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackTestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sweetcherrytech/faller/FullstackTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweetcherrytech.faller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
